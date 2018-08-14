f1273dd4da014080618dd5a6e8347828958552ce
Sultan Alsawaf <sultan@kerneltoast.com>
2018-08-13 19:58:44 -0700

    qpnp-fg-gen3: Limit how frequently fg data can be queried
    Querying the fuelgauge for data is expensive, so doing it frequently can
    result in a significant increase in power consumption.
    
    Limit how frequently data queries can be sent to 5 seconds in order to
    prevent fuelgauge queries from consuming too much power. Only do this while
    there is no USB cable connected to the device, since the charging drivers
    need fresh statistics when there's a USB cable connected.
    
    Change-Id: I35d52e6c172c37a522d8fe93df728c44f491ef71
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>

--- drivers/power/supply/qcom/fg-core.h
+++ drivers/power/supply/qcom/fg-core.h
@@ -401,6 +401,11 @@ static const struct fg_pt fg_tsmc_osc_table[] = {
 	{  90,		444992 },
 };
 
+struct fg_saved_data {
+	union power_supply_propval val;
+	unsigned long last_req_expires;
+};
+
 struct fg_chip {
 	struct device		*dev;
 	struct pmic_revid_data	*pmic_rev_id;
@@ -484,6 +489,7 @@ struct fg_chip {
 	ktime_t			last_delta_temp_time;
 	struct delayed_work	soc_work;
 	struct delayed_work	empty_restart_fg_work;
+	struct fg_saved_data	saved_data[POWER_SUPPLY_PROP_MAX];
 };
 
 /* Debugfs data structures are below */
